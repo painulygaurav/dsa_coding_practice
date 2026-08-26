772a2e3985b605e539ebb414ad4f2455654e11f1
gaurav.painuly <gaurav.painuly@servicenow.com>
2026-08-26 17:19:04 +0530

    Contains duplicate 217 submitted to leetcode

--- A_ContainsDuplicate_217.java
+++ A_ContainsDuplicate_217.java
@@ -1,27 +1,28 @@
 import java.util.HashSet;
+import java.util.Arrays;
 
 public class A_ContainsDuplicate_217 {
     /**
      * Question:
      * Given an integer array "nums", return true if any value appears at least twice in the array, and return
      * 'false' if every element is distinct.
+     * Constraint:
+     * 1 <= nums.length <= 10^5
+     * -10^9 <= nums[i] <= 10^9
      */
     public static void main(String[] args){
-        int[] nums = {1,4,3,5,4};
+        int[] nums = {1,4,3,5,1};
 
-        System.out.println(containsDuplicate(nums));
+        System.out.println(containsDuplicateOptimal(nums));
     }
 
     /**
-     * Brute Force Approach
+     *
      * Time complexity = O(n^2)
-     * Space complexity = O(1)
-     * @param nums array of integers
-     * @return boolean true if array contains any duplicate else false
      */
-    static boolean containsDuplicateBruteForce(int[] nums) {
-        for (int i : nums){
-            for (int j = i+1; j < nums.length; j++){
+    static Boolean containsDuplicateBruteForce(int[] nums){
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
                 if(nums[i] == nums[j]){
                     return true;
                 }
@@ -31,20 +32,29 @@ public class A_ContainsDuplicate_217 {
     }
 
     /**
-     * Using HashSet
-     * Time complexity = O(n)
-     * Space Complexity = O(n)
-     * @param nums array of integers
-     * @return boolean true if array contains any duplicate else false
+     * Time complexity = O(n log n)
+     * Total time = n logn + n.
+     * Since n log n is the higher degree term we are going to consider it. Hence the time complexity O(nlogn)
      */
-    static boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> numsHashSet = new HashSet<>();
-        for (int i : nums){
-            if(numsHashSet.contains(i)){
+    static Boolean containsDuplicateSubOptimal(int[] nums){
+        Arrays.sort(nums); // O(n log n)
+        for(int i = 0; i < nums.length - 1; i++){ //O(n)
+            if(nums[i] == nums[i+1])
                 return true;
-            }
-            numsHashSet.add(i);
         }
         return false;
     }
+    /**
+     * Time complexity = O(n)
+     */
+    static Boolean containsDuplicateOptimal(int[] nums){
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (!seen.add(num))
+                return true;
+        }
+        return false;
+    }
+
+
 }
